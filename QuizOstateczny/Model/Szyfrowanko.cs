260c1bac1bffa39c876ba14107653c8a2653beef
Łukasz Pyszny <lp303249@student.polsl.pl>
2023-05-31 11:53:57 +0200

    kurna dzialaj ty pieprzony gicie, DODANIE szyfrowanka

---
+++ QuizOstateczny/Model/Szyfrowanko.cs
@@ -0,0 +1,19 @@
+﻿namespace QuizOstateczny.Model
+{
+    class Szyfrowanko
+    {
+        public string Szyfrowanie(string tekst)
+        {
+            string zaszyfrowane = "";
+
+            return zaszyfrowane;
+        }
+
+        public string Odszyfrowanie(string zaszyfrowane)
+        {
+            string tekst = "";
+
+            return tekst;
+        }
+    }
+}
