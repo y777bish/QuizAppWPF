a49f7811c7d217dc1911d7fd41893ff89324d2de
Bartosz Smolarz <bartsmo430@student.polsl.pl>
2023-06-25 21:41:50 +0200

    jeszcze button i grid

--- QuizOstateczny/ViewModel/SummarizeViewModel.cs
+++ QuizOstateczny/ViewModel/SummarizeViewModel.cs
@@ -1,12 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
 
 namespace QuizOstateczny.ViewModel
 {
-    class SummarizeViewModel
+    class SummarizeViewModel : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private ICommand closeWindow;
+        public ICommand CloseWindow
+        {
+            get
+            {
+                if (closeWindow == null)
+                    closeWindow = new RelayCommand(
+
+                     (o) =>
+                     {
+                         Application.Current.MainWindow.Close();
+                     }
+                    ,
+                    (o) => true
+                    );
+                return closeWindow;
+            }
+        }
     }
 }
